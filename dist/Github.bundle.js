d64740c0a6bb9680dc4fbced80bee16768251350
zerozoo-front <zerozoo385@gmail.com>
2021-04-14 14:54:39 +0900

    fix index.html local path issue

--- dist/Github.bundle.js
+++ dist/Github.bundle.js
@@ -1,1 +1,22 @@
-(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[518],{839:(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{"use strict";eval('__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(684);\n/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(411);\nvar _templateObject;\n\nfunction _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }\n\n\n\nvar GitHubWrapper = styled_components__WEBPACK_IMPORTED_MODULE_1__/* .default.p */ .ZP.p(_templateObject || (_templateObject = _taggedTemplateLiteral(["\\n  margin: 80px 50px;\\n  text-align: center;\\n"])));\nvar Github = /*#__PURE__*/(0,react__WEBPACK_IMPORTED_MODULE_0__.memo)(function () {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(GitHubWrapper, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {\n    target: "_blank",\n    href: "https://github.com/zerozoo-front",\n    rel: "move to github page"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("i", {\n    "class": "fab fa-github"\n  }), " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("br", null), "github\\uB85C \\uC774\\uB3D9\\uD558\\uAE30")));\n});\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Github);\n\n//# sourceURL=webpack://demo/./components/Github.jsx?')}}]);
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkdemo"] = self["webpackChunkdemo"] || []).push([["Github"],{
+
+/***/ "./components/Github.jsx":
+/*!*******************************!*\
+  !*** ./components/Github.jsx ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ \"./node_modules/styled-components/dist/styled-components.browser.esm.js\");\nvar _templateObject;\n\nfunction _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }\n\n\n\nvar GitHubWrapper = styled_components__WEBPACK_IMPORTED_MODULE_1__.default.p(_templateObject || (_templateObject = _taggedTemplateLiteral([\"\\n  margin: 80px 50px;\\n  text-align: center;\\n\"])));\nvar Github = /*#__PURE__*/(0,react__WEBPACK_IMPORTED_MODULE_0__.memo)(function () {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(GitHubWrapper, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"h1\", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"a\", {\n    target: \"_blank\",\n    href: \"https://github.com/zerozoo-front\",\n    rel: \"move to github page\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"i\", {\n    \"class\": \"fab fa-github\"\n  }), \" \", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"br\", null), \"github\\uB85C \\uC774\\uB3D9\\uD558\\uAE30\")));\n});\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Github);\n\n//# sourceURL=webpack://demo/./components/Github.jsx?");
+
+/***/ })
+
+}]);
